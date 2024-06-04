d7a28b993b3642640a4b22c52a9d362314e53e0c
kodxxl <111854579+kodxxl@users.noreply.github.com>
2024-06-05 00:06:00 +0400

    Update SKbasis.cpp

--- SKbasis.cpp
+++ SKbasis.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
+    const auto mul = [](const auto x){
+        return [x](const auto y){
+            return x * y;
+        };
+    };
+    const auto half = [](const auto x){
+        return x / 2;
+    };
     //-I-----------------------------
     const auto I = [](const auto x){
         return x;
@@ -20,11 +28,26 @@ int main() {
     //------------------------------
     const auto x = 42;
     const auto y = 41;
+    
+    std::cout << "x = 42" << std::endl;
+    std::cout << "y = 41" << std::endl;
+    
+    const auto xmul = mul(x); // _42_mul();
+    
+    std::cout << "xmul(y) = " << xmul(y) << std::endl; // _42_mul(41) = 1722
+    
+    const auto Sxyz_1 = S(mul)(half)(x);
+    std::cout << "S(mul)(half)(x) = " << Sxyz_1 << std::endl;    
+ 
+    // S(mul)(half)(x) = 882
+
+    const auto Sxyz_2 = S(mul)(I(half))(K(x)(3));
+    std::cout << "S(mul)(I(half))(K(x)(3)) = " << Sxyz_2 << std::endl;
 
     const auto Ix = I(x);
     const auto Iy = I(y);
     const auto SKKx = S(K)(K)(x);
-
+    
     if( Ix == SKKx ) std::cout << "Success! : Ix == SKKx" << std::endl;
     if( Iy not_eq SKKx ) std::cout << "Success! : Iy not_eq SKKx" << std::endl;
     
