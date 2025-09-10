af12450aca450e24ea5da56c941709c9b6d4a365
KoD <111854579+kodxxl@users.noreply.github.com>
2025-09-10 23:19:51 +0400

    Update TMachine.cpp

--- TMachine.cpp
+++ TMachine.cpp
@@ -1,12 +1,7 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
+#include <pair>
+#include <tuple>
+#include <vector>
 
 class State {
   private:
